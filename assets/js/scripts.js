96e49b5669c6610e457c5e23d1cea4ac56c3aff3
dakovlen <dakovlen@gmail.com>
2021-10-23 18:17:31 +0300

    Learn if and switch

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -50,6 +50,41 @@
 
 // console.log(isCheced || isClose);
 
+// if (4==9) {
+//   console.log('Ok!')
+// } else {
+//   console.log('Error')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//   console.log('Error');
+// } else if (num > 100) {
+//   console.log('Много')
+// } else {
+//   console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+ 
+const num = '50';
+
+switch (num) {
+  case '49':
+    console.log('Error');
+    break;
+  case '100':
+    console.log('Много');
+    break;
+  case '50':
+    console.log('Ok');
+    break;
+  default:
+    console.log('Не в этот раз');
+    break;
+}
+
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
