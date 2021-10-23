ad739602e754bf2ce6c5c29ce6d7f991be6114bf
dakovlen <dakovlen@gmail.com>
2021-10-23 17:34:21 +0300

    add js

---
+++ assets/js/scripts.js
@@ -0,0 +1,51 @@
+// "use strict";
+// /*let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+
+// var name = "Ivan";
+
+// {
+//   let result = 50;
+//   let newResult = result + 5;
+//   console.log(newResult);
+// }
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// let arr = [
+//     'plum.png',
+//     'orange.png',
+//     'apple.svg',
+// ]
+
+// console.log(arr[1]);*/
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// let incr = 10,
+//     decr = 10;
+
+//     incr++;
+//     decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+const isCheced = true,
+      isClose = false;
+
+console.log(isCheced || isClose);
