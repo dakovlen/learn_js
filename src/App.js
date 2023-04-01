8292eacad41bbf5712f2d46012a316ef314ee68d
Artem Koval <dakovlen@gmail.com>
2023-04-01 19:18:06 +0300

    Add ork state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
+import React, { Component } from 'react'; //подключаю компонент из Реакта 
 import './App.css';
 
-function WhoAmI({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+//Как пропс можно передавать и фунцию.
+//Нейминг пропсы очень важен для дальнейшей разработки, чем понятнее называю, тем удобнее с ней работать в будущем. 
+//Называть надо так, что бы понимать что лежит в нутри компонента.
+//Свойство "key" нужно для определения уникальности компонента. 
+//(что бы Реакт видел нужно ли перерендеривать компонент или нет, менялось ли в нем что-то)
+
+class WhoAmI extends Component { //создаю компонент с именем WhoAmI
+  constructor(props) { //принимаю свойства которые придут с компонентов <WhoAmI></WhoAmI>
+    super(props)//ключевое слово супер
+    this.state = {//
+      yaers: 27,//
+      text: "+++",//
+      position: ''
+
+    }
+  }
+//в фунцию setState передается только один аргумент state (текущее на данный момент состояние). Функция возвращает другой обьект.
+// обернуть в () означает вызвать, заменить слово return
+  nextYear = () => {
+    this.setState (state => ({
+      yaers: state.yaers + 1 
+    }))
+  }
+
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render() { //отвечает за вывод чего-то на странице
+    const { name, surname, link } = this.props
+    const {yaers, position} = this.state
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name}, 
+          surname - {surname}, 
+          age - {yaers}, 
+          position - {position}
+        </h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return "Joan"}} surname="Smith" link="facebook.com"/>
-      <WhoAmI name={() => {return "Alex"}} surname="Nest" link="google.com"/>
+      <WhoAmI name="Joan" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Nest" link="google.com" />
     </div>
   );
 }
